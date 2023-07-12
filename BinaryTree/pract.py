0da5e2311f402af7685458087f8f898297dbfc41
sunnykr1998 <kumar.s@youngsoft.in>
2023-07-12 15:58:44 +0530

    added lca and pract

--- BinaryTree/pract.py
+++ BinaryTree/pract.py
@@ -1,40 +1,71 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, val):
+
+        self.val = val
         self.left = None
         self.right = None
 
+class Tree:
+    # res =[]
 
-class BinaryTree:
-    index = -1
-    def construct_tree(self, node_arr):
+    def construct_tree(self, preorder, inorder):
 
-        self.index += 1
-
-        if self.index >= len(node_arr) or node_arr[self.index] == -1:
-            return None
-
-        new_node = Node(node_arr[self.index])
-
-        new_node.left = self.construct_tree(node_arr)
-        new_node.right = self.construct_tree(node_arr)
-
-        return new_node
-
-    def print_preorder(self, root):
-        if root == None:
+        if not preorder or not inorder:
             return
-        print(root.data,end=" ")
-        self.print_preorder(root.left)
-        self.print_preorder(root.right)
+
+        root = Node(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.construct_tree(preorder[1 : mid+1], inorder[:mid])
+        root.right = self.construct_tree(preorder[mid+1:], inorder[mid+1: ])
+
+        return root
+
+    def traversal(self,root, n, res):
+
+        if root is None:
+            return
+
+
+        if root.val == n:
+            res.append(root.val)
+            return  True
+
+
+
+        if root.val != n and (self.traversal(root.left,n, res) or self.traversal(root.right, n,res)):
+            # print(root.val != n and (self.traversal(root.left,n) or self.traversal(root.right, n)))
+            res.append(root.val)
+            return True
+
+
+
+
+
+        return res
+
+
+
+    # def traversal(self,root, n):
+    #
+    #     if root is None:
+    #         return
+    #
+    #     print(root.val, end=" ")
+    #     self.traversal(root.left)
+    #     self.traversal(root.right)
+    #
+    #     return root
+
 
 if __name__ == '__main__':
-    node = [1, 2, 4, -1, -1, 5, -1, 1, 3, -1, 6, -1, -1]
-    tree = BinaryTree()
-    root = tree.construct_tree(node)
-    tree.print_preorder(root)
+    tree = Tree()
+    preorder =['A', 'B', 'D', 'E', 'C', 'F']
+    inorder = ['D', 'B', 'E', 'A', 'F', 'C']
 
-    # print(root.data)
-
-
-
+    root = tree.construct_tree(preorder, inorder)
+    res =[]
+    r=[]
+    s = tree.traversal(root, 'E', res)
+    rt = tree.traversal(root, 'F', r)
+    print(res)
+    print(r)
